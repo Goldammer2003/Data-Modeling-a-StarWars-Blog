3dcd1fb839db457cbcfbb2d38d2ec373201cafd0
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-25 00:22:23 +0000

    Further progress

--- src/models.py
+++ src/models.py
@@ -41,27 +41,26 @@ def serialize (self):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Planet(Base):
     __tablename__ ="planet"
 
     id:Column (Integrer, primary_key=True)
     name= Column (String (50,) nullable =false, unique=True)
     url = Column (String (150,nullable =True))
-    diameter = Column (String (15,nullable=false))
+    diameter = Column (String (15,nullable=False))
+    population = Column (String (18,nullable=False))
+
+    def serialize (self):
+        return (
+            "id":id.self,
+            "name":name.self,
+            "url": url.self, 
+            "diameter":diameter.self, 
+            "population" population.self 
+        
+        )
+
+        class Character (Base):
 
 
 class Address(Base):
